7cc17aa4f4ddeb7b90e211f04c69495a5c727998
gopalpatil0318 <gopalpatilrcpit@gmail.com>
2024-06-27 10:51:58 +0530

    testinomials website

--- Testinomials-react-app/src/App.jsx
+++ Testinomials-react-app/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
 
   return (
     <>
-     <div className=''>
-      <div>
-        <h1>Our Testinomials</h1>
-        <div></div>
+     <div className='flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold'>Future Engineers</h1>
+        <div className='bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto'></div>
         <Testinomial reviews={reviews}/>
       </div>
      </div>
--- Testinomials-react-app/src/components/Card.jsx
+++ Testinomials-react-app/src/components/Card.jsx
@@ -1,45 +1,35 @@
 import React from 'react'
-import {FaQuoteLeft,FaQuoteRight} from 'react-icons/fa'
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa'
+
 
 const Card = (props) => {
     let review = props.review;
-  return (
-    <div>
-        <div>
-            <img src={review.image} alt="" />
-        </div>
-        <div>
-            <p>{review.name}</p>
-        </div>
-        <div>
-            <p>{review.job}</p>
-        </div>
-        <div>
-            <FaQuoteLeft/>
-        </div>
-        <div>
-            {review.text}
-        </div>
-        <div>
-            <FaQuoteRight/>
-        </div>
+    return (
+        <div className='flex flex-col md:relative'>
+            <div className='absolute top-[-7rem] z-[10] mx-auto '>
+                <img
+                    className='aspect-square rounded-full w-[140px] h-[140] z-[25]'
+                    src={review.image} alt="" />
+                <div className='rounded-full w-[140px] h-[140px] z-[-10] absolute bg-violet-500 top-[-6px] left-[10px]'></div>
+            </div>
+            <div className='text-center mt-7'>
+                <p className='font-bold text-2xl tracking-wide capitalize'>{review.name}</p>
+                <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
+            </div>
+         
+            <div className='text-violet-400 mx-auto mt-5'>
+                <FaQuoteLeft />
+            </div>
+            <div className='text-center mt-4 text-slate-500'>
+                {review.text}
+            </div>
+            <div className='text-violet-400 mx-auto mt-5'>
+                <FaQuoteRight />
+            </div>
 
-        <div>
-            <button>
-                +
-            </button>
-            <button>
-                -
-            </button>
-        </div>
 
-        <div>
-            <button>
-                Surprise Me
-            </button>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Card
